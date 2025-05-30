ecc4d51f4526059cb3d57fd734e3694679159293
aleksbaleks10 <aleks.makharadze@Gmail.com>
2025-05-30 21:33:15 +0400

    second commit

---
+++ level 61/homework/homework.js
@@ -0,0 +1,10 @@
+// 5) ღილაკზე დაკლიკებისას alert box-მა უნდა გამოიტანოს თქვენი ასაკი
+function button(){ 
+    onclick(button)
+    alert(10)
+}
+// 6) როდესაც სურათს დააკლიკებთ, მას უნდა შეეცვალოს src ატრიბუტის მნიშვნელობა
+function picture(){
+    onclick(picture)
+    alert(img="control flow paint")
+}
