d73bc63c7533730ea58a22589b99159a5719e7c8
aleksbaleks10 <aleks.makharadze@Gmail.com>
2025-06-16 14:09:47 +0400

    second commit

---
+++ level 70/homework/homework.js
@@ -0,0 +1,19 @@
+function changeText() {
+  document.getElementById("myParagraph").textContent = "The paragraph has been updated!";
+}
+
+function changeColor() {
+  document.getElementById("myDiv").style.backgroundColor = "orange";
+}
+
+function resizeHeading() {
+  document.getElementById("myHeading").style.fontSize = "40px";
+}
+
+function hideBox() {
+  document.getElementById("myDiv").style.display = "none";
+}
+
+function customAlert() {
+  alert("Hello there! You just triggered an alert!");
+}
