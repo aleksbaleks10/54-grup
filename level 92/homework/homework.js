b4c59b4c8888be8db6a24ba377216ba0a0c26de5
aleksbaleks10 <aleks.makharadze@Gmail.com>
2025-08-16 19:16:47 +0400

    Second commit

---
+++ level 92/homework/homework.js
@@ -0,0 +1,11 @@
+const fruits = ["banana", "orange", "grape", "apple", "kiwi", "apple"];
+const index = fruits.indexOf("apple", 3);
+console.log(index); 
+
+const sentence = "Here we go again.";
+const lastIndex = sentence.lastIndexOf("e");
+console.log(lastIndex); 
+
+const str = "Find the space.";
+const spaceIndex = str.indexOf(" ");
+console.log(spaceIndex); 
