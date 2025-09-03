0b2e26bc89375530856811b15720cb2e27c2e38f
aleksbaleks10 <aleks.makharadze@Gmail.com>
2025-09-03 16:41:13 +0400

    second commit

---
+++ level 92/classwork/classwork.js
@@ -0,0 +1,10 @@
+
+const fruits = ["banana", "orange", "grape", "apple", "kiwi", "apple"];
+const index = fruits.indexOf("apple", 3);
+console.log(index); //3
+const sentence = "Here we go, everyone!";
+const lastIndex = sentence.lastIndexOf("e");
+console.log(lastIndex); // 20
+const str = "Find the space here.";
+const spaceIndex = str.indexOf(" ");
+console.log(spaceIndex); // 4
