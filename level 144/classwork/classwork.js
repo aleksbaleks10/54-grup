6be4cf8dc14884e741ec89eddb023d77561b7c16
aleksbaleks10 <aleks.makharadze@Gmail.com>
2026-02-16 19:49:48 +0400

    second commit

---
+++ level 144/classwork/classwork.js
@@ -0,0 +1,7 @@
+// const process = require("process")
+// process.stdin.on("data", favColor => {
+//     console.log(favColor.toString())
+//     process.stdout.write("ENTER YOUR FAV COLOR ")
+
+// });
+// process.stdout.write("your fav color is :")
